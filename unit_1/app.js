bc460098665ac169dccd5cfcbf7481ba2d64df75
Kai Brown <kai@Kais-MBP-2.hsd1.ma.comcast.net>
2022-04-28 19:33:56 -0400

    started working on JQuery

--- unit_1/app.js
+++ unit_1/app.js
@@ -1,9 +1,19 @@
-
-
-
-
-
+$('nav').append(
+    $(document.createElement('button')).prop({
+        type: 'button',
+        innerHTML: 'Click Me',
+        class: 'btn-styled',
+		click: function () { 
+            $('.sidebar').css('background-color', )
+         }
+	})
+);
 
 $( () => {
-    
-  });
+    $(':button').on('click', () => {
+        $('sidebar').css('background-color', 'red')
+    })
+
+
+    })
+  
